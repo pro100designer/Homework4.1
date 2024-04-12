104cb6be0402d097048ba7919c1d9c5fc549ed61
Александр <kozin.last@yandex.ru>
2024-04-12 21:10:43 +0300

    Исправил замечания!

--- src/Main.java
+++ src/Main.java
@@ -20,16 +20,16 @@ public class Main {
 
         System.out.println("Задача №3");
         int speed = 60;
-        if (speed > 60) {
-            System.out.printf("Если скорость больше %s, то нужно заплатить штраф%n", speed);
+        if (speed <= 60) {
+            System.out.printf("Если скорость равна %s, то можно ездить спокойно%n", speed);
         } else {
-            System.out.printf("Если скорость меньше %s, то штраф платить не нужно%n", speed);
+            System.out.printf("Если скорость равна %s, то придется заплатить штраф%n", speed);
         }
 
         System.out.println("Задача №4");
-        if (age > 2 && age <= 6) {
+        if (age >= 2 && age <= 6) {
             System.out.printf("Если возраст человека равен %s, то ему нужно ходить в садик%n", age);
-        } else if (age > 6 && age < 17) {
+        } else if (age >= 7 && age <= 17) {
             System.out.printf("Если возраст человека равен %s, то ему нужно ходить в школу%n", age);
         } else if (age > 17 && age <= 24) {
             System.out.printf("Если возраст человека равен %s, то ему нужно ходить в университет%n", age);
@@ -43,7 +43,7 @@ public class Main {
             System.out.printf("если возраст ребенка равен %s, то он не может кататься на аттракционе\n", age);
         } else if (age > 5 && age < 14) {
             System.out.printf("если возраст ребенка равен %s, то он  может кататься на аттракционе только в сопровождении врозлого\n", age);
-        } else if (age > 14) {
+        } else if (age >= 14) {
             System.out.printf("если возраст ребенка равен %s, то он  может кататься на аттракционе без сопровождения врозлого\n", age);
 
         }
@@ -51,25 +51,25 @@ public class Main {
 
         System.out.println("Задача №6");
         int amountOfPeople = 102;
-        if (amountOfPeople < 60) {
+        if (amountOfPeople <= 60) {
             System.out.println("Сидячие места есть");
-        }
-        if (amountOfPeople >= 102) {
-            System.out.println("Мест нет!");
-        }
-        if (amountOfPeople <= 102) {
+        } else if (amountOfPeople > 60 && amountOfPeople < 102) {
             System.out.println(" сидячих мест нет но есть стоячие");
+        } else if (amountOfPeople >= 102) {
+            System.out.println("Мест нет!");
         }
 
         System.out.println(" Задача №7");
-        int one = 3;
+        int one = 55;
         int two = 4;
-        int three = 5;
-        if (one > two && two > three) {
+        int three = 22;
+        boolean oneBiggest = one > two && one > three;
+        boolean twoBiggest = two > one && two > three;
+        if (oneBiggest) {
             System.out.println("Первое число больше всех");
-        } else if (two > one && two > three) {
+        } else if (twoBiggest) {
             System.out.println("Второе больше всех");
-        } else if (three > one && three > two) {
+        } else {
             System.out.println("Третье больше всех");
         }
 
